a1b456a64764ba6d7ad7f212087fb830fa427d39
seoldonghun <ehdgns5647@gmail.com>
2024-04-13 14:03:50 +0900

    댓글 작성 및 삭제

---
+++ src/main/java/shop/mtcoding/blog/reply/ReplyRequest.java
@@ -0,0 +1,22 @@
+package shop.mtcoding.blog.reply;
+
+import lombok.Data;
+import shop.mtcoding.blog.board.Board;
+import shop.mtcoding.blog.user.User;
+
+public class ReplyRequest {
+
+    @Data
+    public static class SaveDTO {
+        private Integer boardId; // 댓글을 적은 게시글 번호
+        private String comment; // 댓글 내용
+
+        public Reply toEntity(User sessionUser, Board board){
+            return Reply.builder()
+                    .comment(comment)
+                    .board(board)
+                    .user(sessionUser)
+                    .build();
+        }
+    }
+}
